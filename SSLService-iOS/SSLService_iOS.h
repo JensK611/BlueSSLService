eee271232bc4509a89b1031213e93e394c11547b
Jens Krug <jens.krug@jamitlabs.com>
2017-09-21 19:30:38 +0200

    Add SSLService-iOS shared Framework target

---
+++ SSLService-iOS/SSLService_iOS.h
@@ -0,0 +1,18 @@
+//
+//  SSLService_iOS.h
+//  SSLService-iOS
+//
+//  Created by Jens on 21.09.17.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSLService_iOS.
+FOUNDATION_EXPORT double SSLService_iOSVersionNumber;
+
+//! Project version string for SSLService_iOS.
+FOUNDATION_EXPORT const unsigned char SSLService_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSLService_iOS/PublicHeader.h>
+
+
